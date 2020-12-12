192e0aafe02819d5456204c0c90121c4cd204f04
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-12-12 21:33:42 +0100

    Day 55 - C++
    Solved day 55 task in C++

---
+++ Day 055 - Easy/cpp_solution.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    // "Input number of elements: "
+    int n;
+    cin >> n;
+
+    // Input the elements and square them right away
+    vector<long> nums(n);
+    for (size_t i = 0; i < n; ++i)
+    {
+        cin >> nums[i];
+        nums[i] *= nums[i];
+    }
+    
+    sort(nums.begin(), nums.end());
+    foreach (auto elem : nums)
+        cout << elem << " ";
+    return 0;
+}
